2ff24d6cd83ff68b32ef67f99cc9b3bc8774a5a5
David Sklenář <50031887+TheSkleny@users.noreply.github.com>
2022-07-06 12:07:00 +0200

    Delete zkouska/tryharding directory

--- zkouska/tryharding/modulB1.c
+++
@@ -1,3 +0,0 @@
-// modul1.c
-int x;         // Proměnná x přístupná jiným modulů má hodnotu 0 protože je globání a nuluje se
-extern int y;  // Kompilátor ví že někde v jiným modulu by měla bejt proměnná y
--- zkouska/tryharding/modulC1.c
+++
@@ -1,3 +0,0 @@
-// modul1.c
-extern int x; // Komplátor ví že někde bude proměnná int x buď tady nebo někde jinde
-int x = 55;   // Našel jí tady, pohoda
